c77f97e7607991cdb46e2ca367e4bb7edf92e9c8
pier <petar.stf@gmail.com>
2020-10-12 10:33:18 +0200

    Prod commit

--- 01_functions/get_predictions.R
+++
@@ -1,6 +0,0 @@
-get_predictions <- function(model, data) {
-  as_tibble(h2o.predict(model, as.h2o(data))) %>% 
-    mutate(predict = factor(predict, levels = c(1, 0)),
-           predict05 = factor(ifelse(p1 > 0.5, 1, 0), levels = c(1, 0)),
-           default = data$default)
-}
